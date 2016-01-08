b3069690114f601d85cd5dc64b83469ff2190a9c
Cameron Stokes <cameron@cameronstokes.com>
2016-01-08 10:23:52 -0800

    added debug output

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -4,3 +4,9 @@ variable "environment" {
 output "message" {
   value = "Common config for ${var.environment}."
 }
+
+resource "null_resource" "debug" {
+  provisioner "local-exec" {
+    command = "pwd && tree"
+  }
+}
