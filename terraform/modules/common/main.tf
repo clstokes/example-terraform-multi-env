1bc6ed3578bcdb5b08a3bb23739e16b34933b71b
Cameron Stokes <cameron@cameronstokes.com>
2016-01-08 11:09:16 -0800

    trigger a tf change

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -2,7 +2,7 @@ variable "environment" {
 }
 
 output "message" {
-  value = "Common config for ${var.environment}."
+  value = "Common config for ${var.environment} at 11:09."
 }
 
 resource "null_resource" "debug" {
