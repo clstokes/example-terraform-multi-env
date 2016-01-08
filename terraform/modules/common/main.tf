e12d8f59a8d5233868b8d655aee207381389f7e4
Cameron Stokes <cameron@cameronstokes.com>
2016-01-08 11:53:16 -0800

    empty.tf to allow pushes from root

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -5,7 +5,7 @@ output "message" {
   value = "Common config for ${var.environment}."
 }
 
-resource "null_resource" "debug2" {
+resource "null_resource" "debug" {
   provisioner "local-exec" {
     command = "pwd && find ."
   }
